e742751dd6f178bc8fe611b3a449b0027c7ff540
André Roy <roya0088@uqar.ca>
2020-04-20 21:59:58 -0400

    ADDED: Filtering

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
-const filterOption = document.querySelector(".filter-todo)");
+const filterOption = document.querySelector(".filter-todo");
 
 //TODO: Event Listeners
 todoButton.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 //TODO: Functions
 function addTodo(event) {
@@ -63,5 +64,26 @@ function deleteCheck(event) {
 }
 
 function filterTodo(event) {
-    const todos = todoList.childNodes;
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (event.target.value) {
+      case "all":
+        todo.style.display = "flex";
+        break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if (!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
